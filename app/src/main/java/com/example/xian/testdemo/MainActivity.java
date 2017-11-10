d138797f14bbd9afe6941e5ffccdd3feb79b21d3
kang <541401998@qq.com>
2017-11-10 09:36:27 +0800

    我也不知道那一次的上传，真的

--- app/src/main/java/com/example/xian/testdemo/MainActivity.java
+++ app/src/main/java/com/example/xian/testdemo/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //我现在是更新过的  好的    嗯嗯呢恩
-//        新的是什么呢
+//        新的是什么呢  haha
     }
 }
