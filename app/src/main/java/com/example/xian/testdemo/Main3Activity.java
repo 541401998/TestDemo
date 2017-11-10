1d4f08ca5a9dd0ec4a2e6721a2a9aed2aef798e8
kang <541401998@qq.com>
2017-11-10 09:34:19 +0800

    我的上传

--- app/src/main/java/com/example/xian/testdemo/Main3Activity.java
+++ app/src/main/java/com/example/xian/testdemo/Main3Activity.java
@@ -3,12 +3,11 @@ package com.example.xian.testdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //我现在是更新过的  好的    嗯嗯呢恩
+        setContentView(R.layout.activity_main3);
     }
 }
